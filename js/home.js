bedffb63fae9d9a949927bc3607b91d3576e8a0e
Jiang Sheng <gisonrg@gmail.com>
2015-01-27 02:56:08 +0800

    update the UI greatly.

--- js/home.js
+++ js/home.js
@@ -7,4 +7,32 @@ $(function() {
 		dots: true,               //  Display dot navigation
 		fluid: false,              //  Support responsive design. May break non-responsive designs
 	});
+
+	/**
+	 * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
+	 * @see http://flesler.demos.com/jquery/scrollTo/
+	 * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.LocalScroll.
+	 */
+	
+	// The default axis is 'y', but in this demo, I want to scroll both
+	// You can modify any default like this
+	// $.localScroll.defaults.axis = 'xy';
+	
+	/**
+	 * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
+	 * also affect the >> and << links. I want every link in the page to scroll.
+	 */
+	$.localScroll({
+		target: 'body', // could be a selector or a jQuery object too.
+		queue:true,
+		duration:500,
+		hash:true,
+		onBefore:function( e, anchor, $target ){
+			// The 'this' is the settings object, can be modified
+		},
+		onAfter:function( anchor, settings ){
+			// The 'this' contains the scrolled element (#content)
+		}
+	});
+
 });
